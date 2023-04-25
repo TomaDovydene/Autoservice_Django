a2b7cc71a4420471b36b32e44ddf2d8c9ba4f187
Toma Dovydene <toma.dovydene@gmail.com>
2023-04-25 14:58:38 +0300

    Sukurta admin.py (užsakymo, automobilio, paslaugos  sąrašuose) matomi papildomi stulpeliai - informatyvesnis matymas

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from . import models
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['vehicle', 'date']
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['owner_name', 'vehicle_model', 'plate', 'vin']
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
 # Register your models here.
 admin.site.register(models.VehicleModel)
-admin.site.register(models.Vehicle)
-admin.site.register(models.Order)
+admin.site.register(models.Vehicle, VehicleAdmin)
+admin.site.register(models.Order, OrderAdmin)
 admin.site.register(models.OrderLine)
-admin.site.register(models.Service)
+admin.site.register(models.Service, ServiceAdmin)
