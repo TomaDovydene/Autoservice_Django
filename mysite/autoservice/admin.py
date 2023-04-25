91e85a8b02de996c711557fa9992566eae9f32c3
Toma Dovydene <toma.dovydene@gmail.com>
2023-04-25 15:32:53 +0300

    Į Oder ir virš jo pridėta OrderLineInline, kad būtų galima įrašyti info. Pridėtas OrderLine informat. atvaizdavimas

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from . import models
 
+class OrderLineInline(admin.TabularInline):
+    model = models.OrderLine
+    extra = 0
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['vehicle', 'date']
+    inlines = [OrderLineInline]
+
 
 class VehicleAdmin(admin.ModelAdmin):
     list_display = ['owner_name', 'vehicle_model', 'plate', 'vin']
@@ -11,9 +17,13 @@ class VehicleAdmin(admin.ModelAdmin):
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ['name', 'price']
 
+
+class OrderLineAdmin(admin.ModelAdmin):
+    list_display = ['order', 'service', 'quantity']
+
 # Register your models here.
 admin.site.register(models.VehicleModel)
 admin.site.register(models.Vehicle, VehicleAdmin)
 admin.site.register(models.Order, OrderAdmin)
-admin.site.register(models.OrderLine)
+admin.site.register(models.OrderLine, OrderLineAdmin)
 admin.site.register(models.Service, ServiceAdmin)
