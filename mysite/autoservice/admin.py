1445d36a135ff82818575a505061d4652e8a0f7a
Toma Dovydene <toma.dovydene@gmail.com>
2023-04-25 13:59:09 +0300

    Modeliai registruoti į admin svetainę

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.VehicleModel)
+admin.site.register(models.Vehicle)
+admin.site.register(models.Order)
+admin.site.register(models.OrderLine)
+admin.site.register(models.Service)
