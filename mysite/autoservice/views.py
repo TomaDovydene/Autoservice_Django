9b9f63d98132dbadfb1add292c72d1cd9e38457d
Toma Dovydene <toma.dovydene@gmail.com>
2023-04-26 11:10:19 +0300

    Sukurtas index view'as, settings.py aprasyta templates vieta

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Service, Order, Vehicle
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Autoservice")
+    num_service = Service.objects.all().count()
+    num_order = Order.objects.all().count()
+    num_vehicle = Vehicle.objects.all().count()
+
+    context = {
+        'num_service': num_service,
+        'num_order': num_order,
+        'num_vehicle': num_vehicle
+    }
+
+    return render(request, 'index.html', context=context)
