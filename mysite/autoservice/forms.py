d489b0fc28ca067b0d3b6b2fc8ecb64d449dbceb
Toma Dovydene <toma.dovydene@gmail.com>
2023-05-09 14:09:31 +0300

    Galima profilio redagavimas vartotojui

--- mysite/autoservice/forms.py
+++ mysite/autoservice/forms.py
@@ -1,5 +1,7 @@
 from .models import OrderReview
 from django import forms
+from .models import Profile
+from django.contrib.auth.models import User
 
 class OrderReviewForm(forms.ModelForm):
     class Meta:
@@ -7,3 +9,16 @@ class OrderReviewForm(forms.ModelForm):
         fields = ('content',)
         widgets = {'order': forms.HiddenInput(), 'reviewer': forms.HiddenInput()}
 
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['photo']
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,6 +43,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'autoservice',
     'tinymce',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -172,3 +174,6 @@ TINYMCE_DEFAULT_CONFIG = {
     'statusbar': True,
     }
 
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
